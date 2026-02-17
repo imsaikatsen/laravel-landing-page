aeef963cebb04884bcd2b913db66d02d8c07a99e
Mridul Kar <mridulkar.connect@gmail.com>
2026-02-17 14:54:45 +0600

    admin prefix to all backend routes

--- routes/web.php
+++ routes/web.php
@@ -47,113 +47,120 @@ Route::middleware('guest')->group(function () {
         ->name('password.store');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-        ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
+Route::prefix('admin')->group(function () {
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+        Route::middleware('auth')->group(function () {
+            Route::get('verify-email', EmailVerificationPromptController::class)
+                ->name('verification.notice');
 
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+            Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('verification.verify');
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-        ->name('logout');
+            Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware('throttle:6,1')
+                ->name('verification.send');
+
+            Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+                ->name('password.confirm');
+
+            Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+            Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
+            Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->name('logout');
+        });
+
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->middleware(['auth', 'verified'])->name('dashboard');
+
+        Route::middleware('auth')->group(function () {
+            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        });
+
+        Route::get('/dashboard', function () {
+            return view('dashboard-home');
+        })->middleware(['auth'])->name('dashboard');
+
+
+        Route::middleware(['auth'])->group(function () {
+
+            Route::get('/slider', [SliderController::class, 'index'])->name('slider.index');
+            Route::get('/slider/create', [SliderController::class, 'create'])->name('slider.create');
+            Route::post('/slider/store', [SliderController::class, 'store'])->name('slider.store');
+            Route::get('/slider/{id}/edit', [SliderController::class, 'edit'])->name('slider.edit');
+            Route::post('/slider/{id}/update', [SliderController::class, 'update'])->name('slider.update');
+            Route::post('/slider/{id}/delete', [SliderController::class, 'destroy'])->name('slider.destroy');
+        });
+
+        Route::middleware(['auth'])->group(function () {
+        Route::get('/miniapp', [MiniAppController::class, 'index'])->name('miniapp.index');
+            Route::get('/miniapp/create', [MiniAppController::class, 'create'])->name('miniapp.create');
+            Route::post('/miniapp/store', [MiniAppController::class, 'store'])->name('miniapp.store');
+            Route::get('/miniapp/{id}/edit', [MiniAppController::class, 'edit'])->name('miniapp.edit');
+            Route::put('/miniapp/{id}/update', [MiniAppController::class, 'update'])->name('miniapp.update');
+            Route::delete('/miniapp/{id}/destroy', [MiniAppController::class, 'destroy'])->name('miniapp.destroy');
+        });
+
+        // Route::get('/miniapp/{slug}', [MiniAppController::class, 'show'])->name('miniapp.show');
+
+
+        Route::middleware(['auth'])->group(function () {
+            Route::get('/datingzone', [DatingZoneController::class, 'index'])->name('datingzone.index');
+            Route::get('/datingzone/create', [DatingZoneController::class, 'create'])->name('datingzone.create');
+            Route::post('/datingzone/store', [DatingZoneController::class, 'store'])->name('datingzone.store');
+            Route::get('/datingzone/{id}/edit', [DatingZoneController::class, 'edit'])->name('datingzone.edit');
+            Route::post('/datingzone/{id}/update', [DatingZoneController::class, 'update'])->name('datingzone.update');
+            Route::post('/datingzone/{id}/delete', [DatingZoneController::class, 'destroy'])->name('datingzone.destroy');
+        });
+
+        // Route::get('/datingzone/{slug}', [DatingZoneController::class, 'show'])->name('datingzone.show');
+
+        Route::middleware(['auth'])->group(function () {
+            Route::get('/livezone', [LiveZoneController::class, 'index'])->name('livezone.index');
+            Route::get('/livezone/create', [LiveZoneController::class, 'create'])->name('livezone.create');
+            Route::post('/livezone/store', [LiveZoneController::class, 'store'])->name('livezone.store');
+            Route::get('/livezone/{id}/edit', [LiveZoneController::class, 'edit'])->name('livezone.edit');
+            Route::put('/livezone/{id}/update', [LiveZoneController::class, 'update'])->name('livezone.update');
+            Route::delete('/livezone/{id}/destroy', [LiveZoneController::class, 'destroy'])->name('livezone.destroy');
+        });
+
+        // Route::get('/livezone/{slug}', [LiveZoneController::class, 'show'])->name('livezone.show');
+
+
+        Route::middleware(['auth'])->group(function () {
+            Route::get('/mallproducts', [MallProductController::class, 'index'])->name('mallproducts.index');
+            Route::get('/mallproducts/create', [MallProductController::class, 'create'])->name('mallproducts.create');
+            Route::post('/mallproducts/store', [MallProductController::class, 'store'])->name('mallproducts.store');
+            Route::get('/mallproducts/{id}/edit', [MallProductController::class, 'edit'])->name('mallproducts.edit');
+            Route::put('/mallproducts/{id}/update', [MallProductController::class, 'update'])->name('mallproducts.update');
+            Route::delete('/mallproducts/{id}/destroy', [MallProductController::class, 'destroy'])->name('mallproducts.destroy');
+
+        });
+
+        // Route::get('/mallproducts/{slug}', [MallProductController::class, 'show'])->name('mallproducts.show');
+
+        Route::middleware(['auth'])->group(function () {
+
+            Route::get('/pageseo', [PageSeoController::class,'index'])->name('pageseo.index');
+            Route::get('/pageseo/create', [PageSeoController::class,'create'])->name('pageseo.create');
+            Route::post('/pageseo/store', [PageSeoController::class,'store'])->name('pageseo.store');
+            Route::get('/pageseo/{id}/edit', [PageSeoController::class,'edit'])->name('pageseo.edit');
+            Route::put('/pageseo/{id}/update', [PageSeoController::class,'update'])->name('pageseo.update');
+            Route::delete('/pageseo/{id}/destroy', [PageSeoController::class,'destroy'])->name('pageseo.destroy');
+
+        });
+
+
+        Route::get('/', [LandingPageController::class, 'index']);
+
+        Route::get('吴萌萌/{slug}', [SlugController::class, 'resolve'])->name('slug.resolve');
+
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard-home');
-})->middleware(['auth'])->name('dashboard');
-
-
-Route::middleware(['auth'])->group(function () {
-
-    Route::get('/slider', [SliderController::class, 'index'])->name('slider.index');
-    Route::get('/slider/create', [SliderController::class, 'create'])->name('slider.create');
-    Route::post('/slider/store', [SliderController::class, 'store'])->name('slider.store');
-    Route::get('/slider/{id}/edit', [SliderController::class, 'edit'])->name('slider.edit');
-    Route::post('/slider/{id}/update', [SliderController::class, 'update'])->name('slider.update');
-    Route::post('/slider/{id}/delete', [SliderController::class, 'destroy'])->name('slider.destroy');
-});
-
-Route::middleware(['auth'])->group(function () {
-Route::get('/miniapp', [MiniAppController::class, 'index'])->name('miniapp.index');
-    Route::get('/miniapp/create', [MiniAppController::class, 'create'])->name('miniapp.create');
-    Route::post('/miniapp/store', [MiniAppController::class, 'store'])->name('miniapp.store');
-    Route::get('/miniapp/{id}/edit', [MiniAppController::class, 'edit'])->name('miniapp.edit');
-    Route::put('/miniapp/{id}/update', [MiniAppController::class, 'update'])->name('miniapp.update');
-    Route::delete('/miniapp/{id}/destroy', [MiniAppController::class, 'destroy'])->name('miniapp.destroy');
-});
-
-// Route::get('/miniapp/{slug}', [MiniAppController::class, 'show'])->name('miniapp.show');
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/datingzone', [DatingZoneController::class, 'index'])->name('datingzone.index');
-    Route::get('/datingzone/create', [DatingZoneController::class, 'create'])->name('datingzone.create');
-    Route::post('/datingzone/store', [DatingZoneController::class, 'store'])->name('datingzone.store');
-    Route::get('/datingzone/{id}/edit', [DatingZoneController::class, 'edit'])->name('datingzone.edit');
-    Route::post('/datingzone/{id}/update', [DatingZoneController::class, 'update'])->name('datingzone.update');
-    Route::post('/datingzone/{id}/delete', [DatingZoneController::class, 'destroy'])->name('datingzone.destroy');
-});
-
-// Route::get('/datingzone/{slug}', [DatingZoneController::class, 'show'])->name('datingzone.show');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/livezone', [LiveZoneController::class, 'index'])->name('livezone.index');
-    Route::get('/livezone/create', [LiveZoneController::class, 'create'])->name('livezone.create');
-    Route::post('/livezone/store', [LiveZoneController::class, 'store'])->name('livezone.store');
-    Route::get('/livezone/{id}/edit', [LiveZoneController::class, 'edit'])->name('livezone.edit');
-    Route::put('/livezone/{id}/update', [LiveZoneController::class, 'update'])->name('livezone.update');
-    Route::delete('/livezone/{id}/destroy', [LiveZoneController::class, 'destroy'])->name('livezone.destroy');
-});
-
-// Route::get('/livezone/{slug}', [LiveZoneController::class, 'show'])->name('livezone.show');
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/mallproducts', [MallProductController::class, 'index'])->name('mallproducts.index');
-    Route::get('/mallproducts/create', [MallProductController::class, 'create'])->name('mallproducts.create');
-    Route::post('/mallproducts/store', [MallProductController::class, 'store'])->name('mallproducts.store');
-    Route::get('/mallproducts/{id}/edit', [MallProductController::class, 'edit'])->name('mallproducts.edit');
-    Route::put('/mallproducts/{id}/update', [MallProductController::class, 'update'])->name('mallproducts.update');
-    Route::delete('/mallproducts/{id}/destroy', [MallProductController::class, 'destroy'])->name('mallproducts.destroy');
-
-});
-
-// Route::get('/mallproducts/{slug}', [MallProductController::class, 'show'])->name('mallproducts.show');
-
-Route::middleware(['auth'])->group(function () {
-
-Route::get('/pageseo', [PageSeoController::class,'index'])->name('pageseo.index');
-Route::get('/pageseo/create', [PageSeoController::class,'create'])->name('pageseo.create');
-Route::post('/pageseo/store', [PageSeoController::class,'store'])->name('pageseo.store');
-Route::get('/pageseo/{id}/edit', [PageSeoController::class,'edit'])->name('pageseo.edit');
-Route::put('/pageseo/{id}/update', [PageSeoController::class,'update'])->name('pageseo.update');
-Route::delete('/pageseo/{id}/destroy', [PageSeoController::class,'destroy'])->name('pageseo.destroy');
-
-});
-
-
-Route::get('/', [LandingPageController::class, 'index']);
-
-Route::get('吴萌萌/{slug}', [SlugController::class, 'resolve'])->name('slug.resolve');
