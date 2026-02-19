538ca493e4797d10a04a77d3ec8e80a9c7b6e736
unknown <Paris Ariyan>
2026-02-19 15:42:47 +0600

    added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer('site.includes.header', function ($view) {
-            $menus = DynMainMenu::with(['subMenus'])->where([['location', '=', 'Main Menu']])->get();
-            $view->with('menus', $menus);
+        View::composer('site.layouts.main-layout', function ($view) {
+            $seo = PageSeo::first();
+            $view->with('seo', $seo);
         });
     }
 }
