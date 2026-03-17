96fde4770fb06b85dd803cfbe5f0b3d8c60d1c9d
Paris Ariyan <arianbala56@gmail.com>
2026-03-17 18:04:44 +0600

    Update main-layout.blade.php

--- resources/views/site/layouts/main-layout.blade.php
+++ resources/views/site/layouts/main-layout.blade.php
@@ -28,7 +28,7 @@ body{
 @php
     $isLanding = request()->is('/');
 @endphp
-<link rel="canonical" href="{{ $isLanding ? 'https://ks192.com' : request()->url() }}" />
+<link rel="canonical" href="{{ $isLanding ? 'https://ks192.com' : urldecode(request()->url()) }}" />
 </head>
 <body>
 
